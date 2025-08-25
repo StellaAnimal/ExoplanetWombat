3ff2c903e21bb2a93d1187b1694bbe41b5eaffda
Hajoon Lee <lhj5631572@gmail.com>
2025-08-25 17:56:40 +0900

    Change Commit

---
+++ celebrate_01/celebrate_01_script.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.getElementById('changeName').textContent = '곽건호님 생일 축하해요!';
+    }, 2000); // 1000ms = 1초
+});
